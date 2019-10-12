2fb824c36804556058c48776b8549e7702e2ca9f
catoaune <cato@postoffice.no>
2019-10-12 23:53:42 +0200

    Fixed copy and paste error (args number)

--- cmd/sms/gatewayapi/main.go
+++ cmd/sms/gatewayapi/main.go
@@ -11,7 +11,7 @@ func main() {
 	key := os.Getenv("key")
 	secret := os.Getenv("secret")
 	msg := os.Args[1]
-	receipient, _ := strconv.ParseUint(os.Args[1], 10, 64)
+	receipient, _ := strconv.ParseUint(os.Args[2], 10, 64)
 	gatewayapiConfig := gatewayapi.NewConfig(key, secret, "MChannel")
 	gatewayapiConfig.SendNotification(msg, receipient)
 }
