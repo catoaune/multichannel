ad617e5e0c60a766b3847c1cb2a9a440cc60a356
BuildTools <unconfigured@null.spigotmc.org>
2020-06-08 16:14:37 +0200

    WIP adding support for XML gateway

--- channel/sms/pswincom/pswincom.go
+++ channel/sms/pswincom/pswincom.go
@@ -4,9 +4,9 @@ import (
 	"bytes"
 	"encoding/xml"
 	"errors"
+	"fmt"
 	"log"
 	"net/http"
-	"net/url"
 	"strings"
 )
 
@@ -19,23 +19,27 @@ type Config struct {
 	from       string
 }
 
+type msg  struct {
+	Text string `xml:",chardata"`
+	ID   string `xml:"ID"`
+	message string `xml:"TEXT"`
+	sender  string `xml:"SND"`
+	receiver  string `xml:"RCV"`
+	operation   string `xml:"OP"`
+	class string `xml:"CLASS"`
+}
+
+type msglst struct {
+	Text string `xml:",chardata"`
+	msg `xml:"MSG"`
+}
+
 type smsMessages struct {
 	XMLName xml.Name `xml:"SESSION"`
 	Text    string   `xml:",chardata"`
 	username  string   `xml:"CLIENT"`
 	password      string   `xml:"PW"`
-	msglst  struct {
-		Text string `xml:",chardata"`
-		msg  []struct {
-			Text string `xml:",chardata"`
-			ID   string `xml:"ID"`
-			message string `xml:"TEXT"`
-			sender  string `xml:"SND"`
-			receiver  string `xml:"RCV"`
-			operation   string `xml:"OP"`
-			class string `xml:"CLASS"`
-		} `xml:"MSG"`
-	} `xml:"MSGLST"`
+	msglst `xml:"MSGLST"`
 }
 
 //NewConfig returns a new Config
@@ -45,19 +49,34 @@ func NewConfig(username string, password string, from string) Config {
 }
 
 //SendNotification sends msg to recipient as SMS
-func (c Config) SendNotification(msg string, recipient string) error {
-	requestData := "USER=" + c.username
-	requestData += "&PW=" + c.password
-	requestData += "&RCV=" + formatNumber(recipient)
-	requestData += "&SND=" + c.from
-	requestData += "&TXT=" + url.QueryEscape(msg)
-	log.Println("Req: " + requestData)
+func (c Config) SendNotification(message string, recipient string) error {
+
+	mess := msg{
+		message:   textAsHex(message),
+		sender:    c.from,
+		receiver:  formatNumber(recipient),
+		operation: "9",
+		class:     "3",
+	}
+	list := msglst{
+		msg:  mess,
+	}
+	sms := smsMessages{username: c.username, password: c.password, msglst: list}
+
+//	requestData := "USER=" + c.username
+//	requestData += "&PW=" + c.password
+//	requestData += "&RCV=" + formatNumber(recipient)
+//	requestData += "&SND=" + c.from
+//	requestData += "&TXT=" + url.QueryEscape(msg)
+	log.Printf("Req: %v", sms)
 	client := &http.Client{}
-	b := bytes.NewBuffer([]byte(requestData))
+//	b := bytes.NewBuffer([]byte(requestData))
+	x, _ := xml.Marshal(sms)
+	b := bytes.NewBuffer([]byte(x))
 	request, _ := http.NewRequest("POST", c.URL, b)
 
-	request.Header.Add("Content-Length", string(len(requestData)))
-	request.Header.Add("Content-Type", "application/x-www-form-urlencoded; charset=utf-8")
+	request.Header.Add("Content-Length", string(len(x)))
+	request.Header.Add("Content-Type", "text/xml")
 
 	resp, _ := client.Do(request)
 	if resp.StatusCode < 200 && resp.StatusCode >= 300 {
@@ -66,9 +85,14 @@ func (c Config) SendNotification(msg string, recipient string) error {
 	return nil
 }
 
-func (sm smsMessages) setTextAsHex(text string) {
-
+func textAsHex(text string) string {
+	var hexStr string
+	for _, runeValue := range text {
+		hexStr += fmt.Sprintf("%U", runeValue)
+	}
+	return strings.ReplaceAll(hexStr, "U+", "")
 }
+
 func formatNumber(phoneNumber string) string {
 	formatted := strings.Replace(phoneNumber, "+", "", -1)
 	return strings.Replace(formatted, " ", "", -1)
