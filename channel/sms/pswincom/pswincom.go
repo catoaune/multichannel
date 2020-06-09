2483e165af71db95e02bea47d90f4ab6e9d0225c
BuildTools <unconfigured@null.spigotmc.org>
2020-06-09 12:11:53 +0200

    WIP reenable sending

--- channel/sms/pswincom/pswincom.go
+++ channel/sms/pswincom/pswincom.go
@@ -3,6 +3,7 @@ package pswincom
 import (
 	"bytes"
 	"encoding/xml"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -70,7 +71,7 @@ func (c Config) SendNotification(message string, recipient string) error {
 //	requestData += "&SND=" + c.from
 //	requestData += "&TXT=" + url.QueryEscape(msg)
 
-	//client := &http.Client{}
+	client := &http.Client{}
 //	b := bytes.NewBuffer([]byte(requestData))
 	x, _ := xml.Marshal(sms)
 	log.Printf("Req: %v", string(x))
@@ -80,10 +81,10 @@ func (c Config) SendNotification(message string, recipient string) error {
 	request.Header.Add("Content-Length", string(len(x)))
 	request.Header.Add("Content-Type", "text/xml")
 
-	//resp, _ := client.Do(request)
-	//if resp.StatusCode < 200 && resp.StatusCode >= 300 {
-	//	return errors.New(string(resp.StatusCode) + " " + resp.Status)
-	//}
+	resp, _ := client.Do(request)
+	if resp.StatusCode < 200 && resp.StatusCode >= 300 {
+		return errors.New(string(resp.StatusCode) + " " + resp.Status)
+	}
 	return nil
 }
 
